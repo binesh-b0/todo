5f28d2f881474ea8b74c02566db02f0ec52336d9
binesh <predme@outlook.com>
2019-04-08 03:12:18 +0000

    added count

--- todo.sol
+++ todo.sol
@@ -5,21 +5,24 @@ contract todo{
         string title;
         bool status;
     }
-    
     mapping(uint => tasks) task;
+    uint[] public id; 
     
-    event newTaskAdded(uint id,string memory title,bool status);
+    event newTaskAdded(uint id,string title,bool status);
     
     function newTask(uint _id,string memory _title,bool _status) public {
-        task[_id] = tasks(_title,_status);
+        id.push(_id);
+        task[_id] = tasks(_title,false);
         emit newTaskAdded(_id,_title,_status);
     }
-    function getTask(uint _id) public view returns(string memory _title,bool _status){
+    function getTask(uint _id) public view returns(string memory _title,bool _status,uint count){
         _title = task[_id].title;
         _status = task[_id].status;
+        count=id.length;
     }
     function setStatus(uint _id,bool _status) public {  
         task[_id].status=_status;    
     
     }
+
 }
